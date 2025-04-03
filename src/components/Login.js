343e01519c2dd301024e0346f4de47c810f578d5
Paddy Downey <G00392495@atu.ie>
2025-04-03 11:52:48 +0100

    logina added for authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useAuth } from './AuthContext';
 import './Login.css'; 
 
+
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
