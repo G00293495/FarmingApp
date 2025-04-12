920ef6e92be4c6567f073f2ca347cbcc6b75d921
Paddy Downey <G00392495@atu.ie>
2025-04-12 10:16:32 +0100

    cleanup

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useAuth } from './AuthContext';
 import './Login.css'; 
 
-
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
